921e797f6d3f2c29d24302b3de7c2b55071caf28
JuaumMitty <jjoaovictorpg@gmail.com>
2022-08-02 11:19:20 -0300

    Criando store

--- src/components/BarraLateral.vue
+++ src/components/BarraLateral.vue
@@ -9,13 +9,13 @@
     <nav class="panel mt-5">
       <ul>
         <li>
-          <router-link to="/">
+          <router-link to="/" class="link">
             <i class="fas fa-tasks"></i>
             tarefas
           </router-link>
         </li>
         <li>
-          <router-link to="/projetos">
+          <router-link to="/projetos" class="link">
             <i class="fas fa-project-diagram"></i>
             projetos
           </router-link>
@@ -55,18 +55,40 @@ export default defineComponent({
 </script>
 
 <style scoped>
+h1 {
+  text-align: center;
+}
+
+strong {
+  color: #f95738;
+}
+
 header {
-  padding: 1rem;
   background: #0d3b66;
   width: 100%;
   height: 100vh;
-  text-align: center;
+  padding: 2rem;
 }
 
 @media only screen and (max-width: 768px) {
   header {
-    padding: 2.5rem;
     height: auto;
   }
+}
+
+.panel li {
+  margin: 8px 0;
+}
+
+.link {
+  color: #fff;
+}
+
+.link:hover {
+  color: #FAF0CA;
+}
+
+.link.router-link-active {
+  color: #FAF0CA;
 }
 </style>
--- src/views/ProjetosView.vue
+++ src/views/ProjetosView.vue
@@ -1,14 +1,59 @@
 <template>
-    <h1>Projetos</h1>
+    <section class="projetos">
+        <h1 class="title">Projetos</h1>
+        <form @submit.prevent="salvar">
+            <div class="field">
+                <label for="nomeDoProjeto" class="label">Nome Do Projeto</label>
+                <input type="text" class="input" v-model="nomeDoProjeto" />
+            </div>
+            <div>
+                <button class="button" type="submit">Salvar</button>
+            </div>
+        </form>
+        <table class="table is-fullwidth">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nome</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr v-for="projeto in projetos" :key="projeto.id">
+                    <td>{{ projeto.id }}</td>
+                    <td>{{ projeto.nome }}</td>
+                </tr>
+            </tbody>
+        </table>
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import IProjeto from '../interface/IProjeto'
 
 export default defineComponent({
     name: 'ProjetosView',
+    data() {
+        return {
+            nomeDoProjeto: "",
+            projetos: [] as IProjeto[]
+        }
+    },
+    methods: {
+        salvar() {
+            const projeto: IProjeto = {
+                nome: this.nomeDoProjeto,
+                id: new Date().toISOString()
+            }
+            this.projetos.push(projeto)
+            this.nomeDoProjeto = ''
+        }
+    }
 })
 </script>
 
-<style>
+<style scoped>
+.projetos {
+    padding: 1.25rem
+}
 </style>
